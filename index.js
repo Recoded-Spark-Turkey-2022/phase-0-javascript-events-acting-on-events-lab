2e86f9c768d0ad588c23a15f8a339df89bddc01b
trteel1 <tateelamino@gmail.com>
2023-01-26 20:02:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code here
+const moveDodgerLeft = () => {
+    const num = dodger.style.left.replace("px", "")
+    if(num > 0) { dodger.style.left = num - 1 + "px" }
+}
+const moveDodgerRight = () => {
+    const num = parseInt(dodger.style.left.replace("px", ""));
+    if (num < 360) { dodger.style.left = num + 1 + "px" }
+}
